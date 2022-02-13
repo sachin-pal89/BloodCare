01b31b4808d322864d9a6dc6d12d37ccd8f3114e
Sachin Pal <sachinpal01012003@gmail.com>
2022-02-13 10:56:07 +0530

    Updated files

--- app/src/main/java/com/example/bloodcare/Email/Util.java
+++
@@ -1,6 +0,0 @@
-package com.example.bloodcare.Email;
-
-public class Util {
-    public  static  final String EMAIL = "blooddonationappas@gmail.com";
-    public static final String PASSWORD = "bdaas2021";
-}
